217b51ea8ff8fe72aa59d696fc6360fe8d9ab3e2
RHRolun <rlundber@redhat.com>
2024-01-19 19:00:15 +0100

    Set the right namespace

---
+++ fraud-detection/update_yaml.py
@@ -0,0 +1,18 @@
+import yaml
+
+def namespace_specific_yaml(username):
+    with open("raytest_special.yaml", "r") as stream:
+        try:
+            yaml_info = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            
+    yaml_info["metadata"]["namespace"]=username
+    yaml_info["spec"]["resources"]["GenericItems"][0]["generictemplate"]["metadata"]["namespace"]=username
+    yaml_info["spec"]["resources"]["GenericItems"][1]["generictemplate"]["metadata"]["namespace"]=username
+    yaml_info["spec"]["resources"]["GenericItems"][1]["generictemplate"]["spec"]["rules"][0]["host"]=f'raytest-head-svc.{username}.svc.cluster.local'
+    
+    print(yaml_info)
+    
+    with open('raytest.yaml', 'w') as outfile:
+        yaml.dump(yaml_info, outfile, default_flow_style=False)
